16c4ae796d5ece77d0105450fb4f323cf292e44f
zaitopascual-ship-it <zaitopascual@gmail.com>
2026-07-04 11:24:18 +0800

    Add landing page and root route

--- server.js
+++ server.js
@@ -17,6 +17,9 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json({ limit: '10mb' }));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'landing.html'));
+});
 app.use(express.static('public'));
 
 // ---------- ElevenLabs Client ----------
